1c1cc53812bf6f4afdc444a7b37a10d8fb3e4b32
hiba hmadad <hibahmadad12@gmail.com>
2025-11-03 22:01:43 +0100

    Sauvegarde avant pull

--- Config-Server/src/main/java/ma/gov/projetmicroservice/configserver/ConfigServerApplication.java
+++ Config-Server/src/main/java/ma/gov/projetmicroservice/configserver/ConfigServerApplication.java
@@ -1,13 +1,22 @@
 package ma.gov.projetmicroservice.configserver;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
+@EnableConfigServer
+@Slf4j
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConfigServerApplication.class, args);
-    }
+        ApplicationContext context = SpringApplication.run(ConfigServerApplication.class);
 
+        log.info("\nAccess URLs:\n------------------------------------------------------\n" +
+                        "Url: \thttp://127.0.0.1:{}\n" +
+                        "----------------------------------------------------------",
+                context.getEnvironment().getProperty("server.port"));
+    }
 }
